ba18fced005015e347462ee1e8321c4dcdc671f4
repl.it user <replituser@example.com>
2021-03-08 20:04:32 +0000

    Modulo operator king of game

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Please enter a whole number!")
+number=int(input())
+if(number % 2 == 0):
+  print("\n The number {} is an even number.". format(number))
+else :
+  print("\n The number {} is an odd number.".format(number))
+print("\n\n\t\t End of game!")
